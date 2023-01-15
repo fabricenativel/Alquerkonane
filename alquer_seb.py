ac17ae71fb5aa42f5e4eb299f0af625332a9e27f
Sébastien Hoarau <seb.hoarau@univ-reunion.fr>
2023-01-15 16:34:20 +0400

    modif GameState

--- alquer_seb.py
+++ alquer_seb.py
@@ -3,6 +3,7 @@ Jeu Alquerkonane
 Variante à partir de la version de Fab
 """
 
+from dataclasses import dataclass
 from functools import lru_cache
 import PySimpleGUI as sg
 import argparse
@@ -28,32 +29,33 @@ TURN_MARK = ('▶', ''), ('', '▶')
 WINNING_MARK = ('★', ''), ('', '★')
 RESULTS = ('wins', ''), ('', 'wins')
 
+KEYS = 'Black', 'White'
+ALIGN = 'lr'
+
 # Aide des options
 
 HELP_W = 'Largeur du damier, valeur par défaut 4'
 HELP_H = 'Hauteur du damier, valeur par défaut 4'
 HELP_L = 'Nombre de lignes de pions, 1 ou 2 (par défaut)'
 HELP_WHO_START = "Identifiant du joueur qui commence : 0 = Black, 1 = White (par défaut)"
+HELP_GET_WINNER = "Booléen ; si True le joueur gagnant est calculé et affiché dans les infos"
 
 
 class View:
-
-    KEYS = 'Black', 'White'
-    ALIGN = 'lr'
 
     def __init__(self, controller):
         self.ctrl = controller
         self.height = controller.height
         self.width = controller.width
         # ligne d'informations sur les joueurs noir / blanc : qui joue, qui est gagnant etc.
-        top = [sg.Text("", key=View.KEYS[BLACK], size=(15,1), justification=View.ALIGN[BLACK]),
-               sg.Text("", key=View.KEYS[WHITE], size=(15,1), justification=View.ALIGN[WHITE])]
+        top = [sg.Text("", key=KEYS[BLACK], size=(15,1), justification=ALIGN[BLACK]),
+               sg.Text("", key=KEYS[WHITE], size=(15,1), justification=ALIGN[WHITE])]
 
         # la zone damier
         grid = [[sg.Button('', key=(lig, col), pad=(0, 0)) for col in range(self.width)] for lig in range(self.height)]
 
         # la zone menu
-        menu =[[sg.Button("Undo"), sg.Button("Reset"), sg.Button("Exit"), sg.Button("Who wins?")]]
+        menu =[sg.Button("Undo"), sg.Button("Reset"), sg.Button("Exit")]
 
         # le layout global
         layout = [[top[BLACK], sg.Stretch(), top[WHITE]],
@@ -75,11 +77,11 @@ class View:
         current_winner_txt vaut un des couples de la constante WINNING_MARK ou '-', '-' si l'option n'a pas été choisie
         counts est un le couple des nombres de pions noirs et blancs
         """
-        for color in (BLACK, WHITE):
-            key = View.KEYS[color]  # clé au sens de sg
-            turn_txt = current_player_txt[color]
-            winner_txt = current_winner_txt[color]
-            count = counts[color]
+        for player_id in (BLACK, WHITE):
+            key = KEYS[player_id]  # clé au sens de sg
+            turn_txt = current_player_txt[player_id]
+            winner_txt = current_winner_txt[player_id]
+            count = counts[player_id]
             self.window[key].Update(f'{turn_txt} {key} : {count} {winner_txt}')
 
     def close(self):
@@ -93,62 +95,95 @@ class View:
 class Model:
 
     def __init__(self, controller):
-        self.width = controller.width
-        self.height = controller.height
-        self.lines = controller.lines
+        self.controller = controller
         self.states = [self.initial_state(controller.player_start)]
         self.end = False
 
     def initial_state(self, player_id):
-        height, width = self.height, self.width
-        lines = self.lines
+        height, width, lines = self.controller.game_size()
         if  height % 2 == 0:
-            white = {(height-i-1, j + i%2) for j in range(0, width, 2) for i in range(lines)}
-            black = {(i, j + i%2) for j in range(0, width, 2) for i in range(lines)} 
+            white = {(height-i-1, j + i%2) for j in range(0, width, 2) for i in range(lines) if width > j + i%2}
+            black = {(i, j + i%2) for j in range(0, width, 2) for i in range(lines) if width > j + i%2} 
         else:
             white = {(height-i-1, j + i%2 - 1) for j in range(0, width+1, 2) for i in range(lines) if width > j + i%2 - 1 >= 0}
             black = {(i, j + i%2) for j in range(0, width, 2) for i in range(lines) if width > j + i%2 >=0}
-        return GameState(black, white, player_id)
+        return GameState(width, height, frozenset(black), frozenset(white), player_id)
+
+    def player(self):
+        return self.states[-1].player
+
+    def state(self):
+        return self.states[-1]
+
+    def scores(self):
+        state = self.state()
+        return len(state.black), len(state.white)
+
+    def valid(self, i, j):
+        state = self.state()
+        player = state.player
+        positions = state.black, state.white
+        pawns, ennemies, moves = positions[player], positions[1 - player], MOVES[player]
+        if (i, j) not in pawns:
+            return False
+        candidates = state.get_moves_from(i, j, ennemies, moves)
+        return len(candidates) > 0
+
+    def undo(self):
+        if self.states:
+            self.states.pop()
+
+    def play(self, move):
+        state = self.state().new_state(move)
+        self.states.append(state)
+        player = state.player
+        positions = state.black, state.white
+        if len(positions[player]) == 0 or len(state.get_moves()) == 0:
+            self.end = True
+
+    def get_moves_from(self, i, j):
+        state = self.state()
+        player = state.player
+        positions = state.black, state.white
+        ennemies, moves = positions[1 - player], MOVES[player]
+        return state.get_moves_from(i, j, ennemies, moves)
+
+    def winner(self):
+        return self.state().winner()
+
+
+@dataclass(frozen=True)
+class GameState:
+    """Un état du jeu d'alquerkonane', les attributs sont les coordonnées des pions noirs/blancs et un entier 0, 1 pour le joueur courant"""
+
+    width: int
+    height: int
+    black: frozenset
+    white: frozenset
+    player: int
 
     def inside(self, i, j):
         return 0 <= i < self.height and 0 <= j < self.width
 
     def empty(self, i, j):
-        state = self.states[-1]
-        black, white = state.players
-        return self.inside(i, j) and (i, j) not in black | white
+        return self.inside(i, j) and (i, j) not in self.black | self.white
 
-    def player(self):
-        return self.states[-1].player
-
-    def counts(self):
-        state = self.states[-1]
-        return len(state.players[BLACK]), len(state.players[WHITE])
-
-    def get_moves(self, state=None):
-        """renvoie la liste des mouvements possibles pour un état de jeu sous la forme d'un ensemble de tuples 
-        (un triplet pour une prise, un couple pour un mouvement). Le premier élément du tuple est le nouvel emplacement du pion. 
-        L'autre (ou les deux autres) sont les pions qui disparaissent suite au mouvement
+    def get_moves(self):
+        """renvoie la liste des mouvements possibles sous la forme d'un ensemble de triplets
+        Le premier élément du triplet est le nouvel emplacement du pion. 
+        Les deux autres sont les pions qui disparaissent suite au mouvement : le premier est donc l'ancienne
+        position du pion qui bouge et le 2e est le pion adverse pris et donc peut-être à None si le mouvement n'est pas une prise
         """
-        if state is None:
-            state = self.states[-1]
-        player = state.player
+        positions = self.black, self.white
+        player = self.player
         possible_moves = set()
-        pawns, ennemies, moves = state.players[player], state.players[1 - player], MOVES[player]
+        pawns, ennemies, moves = positions[player], positions[1 - player], MOVES[player]
         for i, j in pawns:
             possible_moves |= self.get_moves_from(i, j, ennemies, moves)
         return possible_moves
 
-    def get_moves_from(self, i, j, ennemies=None, moves=None):
-        """renvoie la liste des mouvements possibles pour le pion en i, j sous la forme d'un ensemble de tuples 
-        (un triplet pour une prise, un couple pour un mouvement). Le premier élément du tuple est le nouvel emplacement du pion. 
-        Les deux autres (le 2e étant éventuellement None, None) sont les pions qui disparaissent suite au mouvement
-        """
-        if ennemies is None:
-            state = self.states[-1]
-            player = state.player
-            ennemies, moves = state.players[1 - player], MOVES[player]
-
+    def get_moves_from(self, i, j, ennemies, moves):
+        """renvoie la liste des mouvements possibles pour le pion en i, j sous la même forme que get_moves"""
         possible_moves = set()
         # déplacements possibles
         for di, dj in moves:
@@ -163,64 +198,34 @@ class Model:
                 possible_moves.add(((end_i, end_j), (i, j), (i+di//2, j+dj//2)))
         return possible_moves
 
-    def new_state(self, move, state):
+    def new_state(self, move):
         ''' Génération d'un nouvel état du jeu en jouant un coup'''
         new_position, pawn_1, pawn_2 = move
-        player = state.player
-        pawns, ennemies = state.players[player], state.players[1 - player]
-        new_pawns = pawns - {pawn_1} | {new_position}
-        new_ennemies = ennemies - {pawn_2}
+        positions = self.black, self.white
+        player = self.player
+        pawns, ennemies = positions[player], positions[1 - player]
+        new_pawns = pawns - {pawn_1} | {new_position} # frozenset 
+        new_ennemies = ennemies - {pawn_2}            # frozenset 
         if player == BLACK:
-            return GameState(new_pawns, new_ennemies, WHITE)
+            return GameState(self.width, self.height, new_pawns, new_ennemies, WHITE)
         else:
-            return GameState(new_ennemies, new_pawns, BLACK)
-    
-    def valid(self, i, j):
-        state = self.states[-1]
-        player = state.player
-        pawns, ennemies, moves = state.players[player], state.players[1 - player], MOVES[player]
-        if (i, j) not in pawns:
-            return False
-        candidates = self.get_moves_from(i, j, ennemies, moves)
-        return len(candidates) > 0
-    
-    def undo(self):
-        if self.states:
-            self.states.pop()
-
-    def play(self, move):
-        state = self.new_state(move, self.states[-1])
-        self.states.append(state)
-        player = state.player
-        if len(state.players[player]) == 0 or len(self.get_moves(state)) == 0:
-            self.end = True
-
-    def last(self):
-        return self.states[-1]
+            return GameState(self.width, self.height, new_ennemies, new_pawns, BLACK)
 
     @lru_cache(maxsize=None)
-    def winner(self, state):
-        player = state.player
-        moves = self.get_moves(state)
+    def winner(self):
+        player = self.player
+        moves = self.get_moves()
         if len(moves) == 0:
             return 1 - player
-        next_states = set()
+        states_to_explore = set()
         for m in moves:
-            n_state = self.new_state(m, state)
-            if len(self.get_moves(n_state)) == 0: 
+            next_state = self.new_state(m)
+            if len(next_state.get_moves()) == 0: 
                 return player
-            next_states.add(n_state)
-        if all(self.winner(s) == 1 - player for s in next_states):
+            states_to_explore.add(next_state)
+        if all(state.winner() == 1 - player for state in states_to_explore):
             return 1 - player
         return player
-
-
-class GameState:
-    """Un état du jeu d'alquerkonane', les attributs sont les coordonnées des pions noirs/blancs et un entier 0, 1 pour le joueur courant"""
-
-    def __init__(self, black, white, player):
-        self.players = frozenset(black), frozenset(white) 
-        self.player = player
 
 
 class Alquerkonane:
@@ -234,6 +239,7 @@ class Alquerkonane:
         self.player_start = WHITE
         self.model = None # initialisé plus tard avec le setup
         self.view = None  # initialisé plus tard avec le setup
+        self.get_winner = False
         self.future_winner = None
         self.end = False
         self.selected = None # pour l'UI: indique donne les coordonnées du pion sélectionné
@@ -245,6 +251,7 @@ class Alquerkonane:
         parser.add_argument('-H', '--height', help=HELP_H, type=int)
         parser.add_argument('-l', '--lines', help=HELP_L, type=int)
         parser.add_argument('-s', '--start', help=HELP_WHO_START)
+        parser.add_argument('--win', help=HELP_GET_WINNER, action="store_true")
 
         args = parser.parse_args()
         if args.width:
@@ -257,14 +264,16 @@ class Alquerkonane:
             self.lines = 1
         if args.start and args.start in '01':
             self.player_start = int(args.start)
+        if args.win:
+            self.get_winner = True
             
     def set_view(self, end=False):
         content = {(i, j): EMPTY_FILES[(i + j)%2] for i in range(self.height) for j in range(self.width)}
-        state = self.model.states[-1]
+        state = self.model.state()
         player = state.player
-        for color in (BLACK, WHITE):
-            for i, j in state.players[color]:
-                content[i, j] = PAWN_FILES[color]
+        for player_id, positions in enumerate((state.black, state.white)):
+            for i, j in positions:
+                content[i, j] = PAWN_FILES[player_id]
         if self.selected is not None:
             content[self.selected] = SELECTED_FILES[player]
             for landing_position in self.landing:
@@ -276,23 +285,25 @@ class Alquerkonane:
     def set_text(self, end):
         player = self.model.player()
         if end:
-            current_players_txt = '', ''
+            current_positions_txt = '', ''
             future_winner_txt = RESULTS[1 - player]
         else:
-            current_players_txt = TURN_MARK[player]
+            current_positions_txt = TURN_MARK[player]
             if self.future_winner is not None:
                 future_winner_txt = WINNING_MARK[self.future_winner]
                 self.future_winner = None
             else:
                 future_winner_txt = '-', '-'
-        counts = self.model.counts() 
-        self.view.set_text(current_players_txt, future_winner_txt, counts)
+        self.view.set_text(current_positions_txt, future_winner_txt, self.model.scores())
 
     def reset(self):
         self.model = Model(self)
         self.set_view()
         self.selected = None # pour l'UI: indique les coordonnées du pion sélectionné
         self.landing = {} # pour l'UI : atterrissage possible d'un pion sélectionné
+
+    def game_size(self):
+        return self.width, self.height, self.lines
 
     def select(self, i, j):
         self.selected = i, j
@@ -311,6 +322,7 @@ class Alquerkonane:
         elif self.selected is not None and (i, j) in self.landing:
             move = self.landing[i, j]
             self.model.play(move)
+            self.future_winner = self.model.winner() if self.get_winner else None
             self.deselect()
 
     def loop(self):
@@ -321,11 +333,10 @@ class Alquerkonane:
                 self.end = True
             elif event == 'Reset':
                 self.reset()
-            elif event == 'Who wins?':
-                self.future_winner = self.model.winner(self.model.last())
             elif not self.model.end:
                 if event == 'Undo':
                     self.model.undo()
+                    self.future_winner = self.model.winner() if self.get_winner else None
                 else:
                     self.handle_click(event)
             self.set_view(self.model.end)
@@ -334,9 +345,9 @@ class Alquerkonane:
         # initialisation du modèle et de la vue
         self.model = Model(self)
         t_start =  perf_counter()
-        self.future_winner = self.model.winner(self.model.last())
+        self.future_winner = self.model.winner()
         perf = perf_counter() - t_start
-        print("La position est gagnante pour ", View.KEYS[self.future_winner])
+        print("La position est gagnante pour ", KEYS[self.future_winner])
         print(f"Calcul en {perf}s")
         self.view = View(self)
         self.set_view() 
